9ada3ba081b20a79d5392173b7f7769f83b0ad36
Noble Emeka <nobioma1@gmail.com>
2019-02-01 00:57:35 +0100

    [feature  #163612596] REST Endpoint to create party

--- routes/partiesRoute.js
+++ routes/partiesRoute.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/', PartyController.getAllParties);
 router.get('/:id', PartyController.getAParty);
+router.post('/', PartyController.createParty);
 
 export default router;
