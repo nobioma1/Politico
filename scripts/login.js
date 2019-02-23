704de85a157b42515bc220cbcae5dd79c5f5d61b
Noble Emeka <nobioma1@gmail.com>
2019-02-23 23:50:24 +0100

    Error on Redirect 4

--- scripts/login.js
+++ scripts/login.js
@@ -34,9 +34,9 @@ async function loginUser(event) {
         }%${expires}%path=/`;
         // Redirects User depending in role
         if (data.data[0].user.isAdmin === true) {
-          window.location = "Politico/pages/admin.html";
+          window.location = "/pages/admin.html";
         } else {
-          window.location = "Politico/pages/user.html";
+          window.location = "/pages/user.html";
         }
       }
     })
