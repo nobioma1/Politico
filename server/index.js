34cc7c3a8f1a510bb5e27d2507bc0a8dbb4e4832
Noble Emeka <nobioma1@gmail.com>
2019-02-28 08:39:43 +0100

    adding swagger documentation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import 'babel-polyfill';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
 import routes from './src/routes/index';
 
 dotenv.config();
@@ -15,6 +17,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Welcome to Politico v1');
